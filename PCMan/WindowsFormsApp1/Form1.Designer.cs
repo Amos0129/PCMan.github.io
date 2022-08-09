018410da6f30bb41213f5695a852881f4a6aacf1
Amos0129 <110878143+Amos0129@users.noreply.github.com>
2022-08-10 02:05:32 +0800

    version1

---
+++ PCMan/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
+            this.button3.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button3.Location = new System.Drawing.Point(306, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "暫停音樂";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button5.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button5.Location = new System.Drawing.Point(306, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "遊戲開始";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 15);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox3.Location = new System.Drawing.Point(89, 139);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox4.Location = new System.Drawing.Point(150, 181);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox5.Location = new System.Drawing.Point(211, 139);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 50;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(0, 306);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(215, 42);
+            this.axWindowsMediaPlayer2.TabIndex = 4;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 354);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(215, 53);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(306, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "播放音樂";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScrollMargin = new System.Drawing.Size(431, 477);
+            this.ClientSize = new System.Drawing.Size(380, 300);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PCMan/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int[,] map = new int[15, 15]
+        { { 0,0,0,0,0,0,0,3,0,0,0,0,0,0,0},
+          { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+          { 0,1,0,0,0,0,0,1,0,0,0,0,0,1,0},
+          { 0,1,0,0,0,0,0,1,0,0,0,0,0,1,0},
+          { 0,1,0,0,1,1,1,1,1,1,1,0,0,1,0},
+          { 0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+          { 0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+          { 5,1,1,1,1,1,1,1,1,1,1,1,1,1,6},
+          { 0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+          { 0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+          { 0,1,0,0,1,1,1,1,1,1,1,0,0,1,0},
+          { 0,1,0,0,0,0,0,1,0,0,0,0,0,1,0},
+          { 0,1,0,0,0,0,0,1,0,0,0,0,0,1,0},
+          { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+          { 0,0,0,0,0,0,0,4,0,0,0,0,0,0,0},
+        };
+        Random a = new Random();
+        int time = 0;
+        int time1 = 0;
+        int gx1=1, gy1=1;
+        int gx2=1, gy2=1;
+        int gx3, gy3;
+        int new_direct = 0, direct = 0;
+        int px = 1, py = 1;
+        int dx = 20, dy = 20;
+        int coin = 89;
+        Image stree, block, stree2;
+        Bitmap bmp;
+        Graphics g;
+        int ghostmove1 = 0, h1 = 0;
+        int ghostmove2 = 0, h2 = 0;
+        int ghostmove3 = 0, h3 = 0;
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            gx3 = pictureBox5.Location.X / 20; gy3 = pictureBox5.Location.Y / 20;
+            if (h3 == 0)
+                ghostmove3++;
+
+            switch (ghostmove3)
+            {
+                case 1:
+                    if ((map[gy3 + 1, gx3] == 0) || (map[gy3 + 1, gx3] == 3))
+                        return;
+                    if (py > gy3)
+                        pictureBox5.Top += 20;
+                    break;
+
+                case 2:
+                    if ((map[gy3 - 1, gx3] == 0) || (map[gy3 - 1, gx3] == 4))
+                        return;
+                    if (py < gy3)
+                        pictureBox5.Top -= 20;
+                    break;
+                case 3:
+                    if ((map[gy3, gx3 + 1] == 0) || (map[gy3, gx3 + 1] == 6))
+                        return;
+                    if (px > gx3)
+                        pictureBox5.Left += 20;
+                    break;
+                case 4:
+                    if ((map[gy3, gx3 - 1] == 0) || (map[gy3, gx3 - 1] == 5))
+                        return;
+                    if (px < gx3)
+                        pictureBox5.Left -= 20;
+                    break;
+            }
+            if (ghostmove3 > 4)
+                ghostmove3 = 0;
+            if ((gy3 == py) && (gx3 == px))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                MessageBox.Show("you are loser like yor life");
+            }
+        }
+        
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (h2 == 0)
+            {
+                ghostmove2 = a.Next(0, 4);
+            }
+            gx2 = pictureBox4.Location.X / 20; gy2 = pictureBox4.Location.Y / 20;
+            time1++; 
+            switch (ghostmove2)
+            {
+                case 0:
+                    {
+                        if (time1 < 2)
+                        {
+                            if ((map[gy2 - 1, gx2] == 0) || (map[gy2 - 1, gx2] == 3))
+                                return;
+                            
+                            pictureBox4.Top -= 20;                         
+                        }
+
+                        else
+                        {
+                            ghostmove2 = a.Next(0, 4);
+                            time1 = 0;
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        if (time1 < 2)
+                        {
+                            if ((map[gy2 + 1, gx2] == 0) || (map[gy2 + 1, gx2] == 4))
+                                return;
+                            pictureBox4.Top += 20;
+                        }
+
+                        else
+                        {
+                            ghostmove2 = a.Next(0, 4);
+                            time1 = 0;
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        if (time1 < 2)
+                        {
+                            if ((map[gy2, gx2 - 1] == 0) || (map[gy2, gx2 - 1] == 5))
+                                return;
+                            pictureBox4.Left -= 20;
+                        }
+
+                        else
+                        {
+                            ghostmove2 = a.Next(0, 4);
+                            time1 = 0;
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        if (time1 < 2)
+                        {
+                            if ((map[gy2, gx2 + 1] == 0) || (map[gy2, gx2 + 1] == 6))
+                                return;
+                            pictureBox4.Left += 20;
+                        }
+
+                        else
+                        {
+                            ghostmove2 = a.Next(0, 4);
+                            time1 = 0;
+                        }
+                        break;
+                    }
+            }
+
+            if ((gy2==py) && (gx2==px))                 
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                MessageBox.Show("you are loser like yor life");               
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (h1 == 0)
+            {
+                ghostmove1 = a.Next(0, 4);
+            }
+
+            gx1 = pictureBox3.Location.X / 20; gy1 = pictureBox3.Location.Y / 20;
+
+            time++;
+
+            switch (ghostmove1)
+            {
+                case 0:
+                    {
+                        if (time < 3)
+                        {
+                            if ((map[gy1 - 1, gx1] == 0) || (map[gy1 - 1, gx1] == 3))
+                                return;
+                            pictureBox3.Top -= 20;
+                        }
+
+                        else
+                            ghostmove1 = a.Next(0, 4);
+                        break;
+                    }
+                case 1:
+                    {
+                        if (time < 3)
+                        {
+                            if ((map[gy1 + 1, gx1] == 0) || (map[gy1 + 1, gx1] == 4))
+                                return;
+                            pictureBox3.Top += 20;
+                        }
+
+                        else
+                            ghostmove1 = a.Next(0, 4);
+                        break;
+                    }
+                case 2:
+                    {
+                        if (time < 3)
+                        {
+                            if ((map[gy1, gx1 - 1] == 0) || (map[gy1, gx1 - 1] == 5))
+                                return;
+                            pictureBox3.Left -= 20;
+                        }
+
+                        else
+                            ghostmove1 = a.Next(0, 4);
+                            
+                        break;
+                    }
+                case 3:
+                    {
+                        if (time < 3)
+                        {
+                            if ((map[gy1, gx1 + 1] == 0) || (map[gy1, gx1 + 1] == 6))
+                                return;
+                            pictureBox3.Left += 20;
+                        }
+
+                        else
+                            ghostmove1 = a.Next(0, 4);
+                        break;
+                    }
+            }
+            if (time > 3)
+                time = 0;
+
+            if ((gy1 == py) && (gx1 == px))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                MessageBox.Show("you are loser like yor life");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play(); //播放
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause(); //暫停
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "bgm.mp3"; //加入歌曲
+            axWindowsMediaPlayer1.Ctlcontrols.play(); //播放
+            axWindowsMediaPlayer1.settings.volume = 35;
+            axWindowsMediaPlayer2.settings.volume = 20;
+            for (int y = 0; y < 15; y++)
+            {
+                for (int x = 0; x < 15; x++)
+                {
+                    if (map[y, x] == 0)
+                        g.DrawImage(block, x * dx, y * dy);
+                    if (map[y, x] == 3 || map[y, x] == 4 || map[y, x] == 5 || map[y, x] == 6)
+                        g.DrawImage(stree2, x * dx, y * dy);
+                    if (map[y, x] == 1)
+                        g.DrawImage(stree, x * dx, y * dy);
+                }
+            }
+            pictureBox1.Image = bmp;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    {
+                        if (map[py, px] == 5)
+                        {
+                            py = 7;
+                            px = 15;
+                        }
+                        if (map[py, px - 1] == 0)                     
+                            return;
+
+                        px--;
+                        new_direct = 2;
+                        break;
+                    }
+                case Keys.D:
+                    {
+                        if (map[py, px] == 6)
+                        {
+                            py = 7;
+                            px = -1;
+                        }
+                        if (map[py, px + 1] == 0)
+                            return;
+
+                        px++;
+                        new_direct = 4;
+                        break;
+                    }
+                case Keys.W:
+                    {
+                        if (map[py, px] == 3)
+                        {
+                            py = 15;
+                            px = 7;
+                        }
+                        if (map[py - 1, px] == 0)
+                            return;
+                        
+                        py--;
+                        new_direct = 1;
+                        break;
+                    }
+                case Keys.S:
+                    {
+                  
+                        if (map[py, px] == 4)
+                        {
+                            py = -1;
+                            px = 7;
+                        }
+                        if (map[py + 1, px] == 0)
+                            return;
+
+                        py++;
+                        new_direct = 3;
+                        break;
+                    }
+            }
+
+            if (map[py, px] == 1)
+            {
+                map[py, px] = 2;
+                g.DrawImage(stree2, px * dx, py * dy);
+                axWindowsMediaPlayer2.URL = "eat.mp3";
+                coin--;
+            }
+
+            if (direct != new_direct)
+            {
+                direct = new_direct;
+                pictureBox2.Image = Image.FromFile("pc" + direct.ToString() + ".png");
+            }
+
+            pictureBox2.Left = px * dx;
+            pictureBox2.Top = py * dy;
+
+            if (coin == 0)
+            {
+                timer2.Enabled = false;
+                timer1.Enabled = false;
+                timer3.Enabled = false;
+                MessageBox.Show("勝利");
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            block = Image.FromFile("block.png");
+            stree = Image.FromFile("stree.png");
+            stree2 = Image.FromFile("stree2.png");
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox4.Location = new Point(dx * 4, dy * 4);
+            pictureBox5.Location = new Point(dx * 7, dy * 4);
+            pictureBox3.Location = new Point(dx * 10, dy * 4);
+            pictureBox2.Location = new Point(dx * 1, dy * 1);
+        }
+    }
+}
